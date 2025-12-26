c82d0df7951249b6afe2f212e3b5b4824e510a2d
MYTsou <pasta1206@yahoo.com.tw>
2025-12-26 23:50:14 +0800

    add deploy.yml

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace MyPortfolioApi.Data;
 // 繼承自 DbContext
 public class AppDbContext : DbContext
 {
-    // 建構子：接收設定選項 (例如連線字串)，並傳給父類別
+    // 建構子：接收設定選項 (例如連線字串)，並傳給父類別 #
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
